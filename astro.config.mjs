ef37b3c5b4f36df78e1928b05c6843019793ee2c
Raspberry Pi <raspberrypisig@gmail.com>
2025-05-12 12:13:38 +1000

    add intro section

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,10 @@ export default defineConfig({
 			],
 			sidebar: [
 				{
+					label: 'Introduction',
+					link: 'index',
+				},
+				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
